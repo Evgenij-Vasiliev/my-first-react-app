b270b0a7ea0fe2a3a79e076efa1e0227aca38901
Evgenij-Vasiliev <vasiliev.evgenij.aleksandrovich@gmail.com>
2022-09-24 10:58:42 +0300

    added ClassCounter

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import React, { useState } from "react";
+import ClassCounter from "./components/ClassCounter";
+import Counter  from "./components/Counter";
 
-function App() {
-  const [likes, setLikes]= useState(0);
-  function increment() {
-    setLikes(likes + 1) 
-
-  }
-
-  function decrement() {
-    setLikes(likes - 1) 
-    
-  }
+function App() {  
+  const [value, setValue]= useState('Текст в инпуте');
+  
   return (
-    <div className="App"> 
-    <h1>{likes}</h1>
-    <button onClick = {increment}>Increment</button>     
-    <button onClick = {decrement}>Decrement</button>     
+    <div className="App">
+      <ClassCounter />                
     </div>
   );
 }
